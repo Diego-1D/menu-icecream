159d688c8ff8f3bd106e37157058e88079ecf769
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-09-30 23:28:55 -0300

    Atualização nos dados

--- src/pages/Home/content.js
+++ src/pages/Home/content.js
@@ -48,61 +48,182 @@ const BorderWhite = styled.div`
     height: 180px;
     background: ${props => props.theme.border.backgroundBorderWhite};
 `;
+const ProductBtnArea = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+const ProductBtn = styled.div`
+    font-size: 20px;
+    padding: 0.75rem 2rem;
+    border: none;
+    border-radius: 40px;
+    background-color:#FF819F;
+    color: #FFFFFF;
+    font-weight: bold;
+    transition: 0.3s ease-out;
+
+    &:hover{
+        cursor: pointer;
+        background: #71CDFF;
+        color: #FFF;
+        transition: 0.3s ease-out;
+    }
+`;
+
+const ProductBtn1 = styled.div`
+    font-size: 20px;
+    padding: 0.75rem 2rem;
+    border: none;
+    border-radius: 40px;
+    background-color:#FFFFFF ;
+    color: #71CDFF;
+    font-weight: bold;
+    transition: 0.3s ease-out;
+
+    &:hover{
+        cursor: pointer;
+        background: #FF819F;
+        color: #FFF;
+        transition: 0.3s ease-out;
+    }
+`;
+
+const ProductBtn2 = styled.div`
+    font-size: 20px;
+    padding: 0.75rem 2rem;
+    border: none;
+    border-radius: 40px;
+    background-color:#FFFFFF ;
+    color: #FF819F;
+    font-weight: bold;
+    transition: 0.3s ease-out;
+
+    &:hover{
+        cursor: pointer;
+        background: #71CDFF;
+        color: #FFF;
+        transition: 0.3s ease-out;
+    }
+`;
 
 export const sections = [
     {
         theme: ContainerWhite,
         border: BorderPink,
-        title: 'Milk Shake',
+        btn: ProductBtn,
+        btnArea: ProductBtnArea,
+        category: 'Milk Shake',
         products: [{
+            id: 1,
             img: milk1,
             alt: 'Milk Shek',
             name: 'Name 01',
-            price: 'R$19.99'
+            price: 'R$19.99',
+            
+           
+                ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+    
         },
         {
+            id: 2,
             img: milk2,
             alt: 'Milk Shek',
             name: 'Name 02',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 3,
             img: milk3,
             alt: 'Milk Shek',
             name: 'Name 03',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 4,
             img: milk4,
             alt: 'Milk Shek',
             name: 'Name 04',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         }
         ],
         allProductsUrl: '/',
-        qtItemsGrid: 4
+        qtItemsGrid: 4,
+       
     },
     {
         theme: ContainerBlue,
         border: BorderWhite,
-        title: 'Sorvete de Pote',
+        btn: ProductBtn1,
+        btnArea: ProductBtnArea,
+        category: 'Sorvete de Pote',
         products: [{
+            id: 1,
             img: pote1,
             alt: 'Açaí com tapioca',
             name: 'Açaí com tapioca',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 2,
             img: pote2,
             alt: 'Creme',
             name: 'Creme',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 3,
             img: pote3,
             alt: 'Chocolate com flocos',
             name: 'Chocolate com flocos',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         }
         ],
         allProductsUrl: '/',
@@ -111,20 +232,32 @@ export const sections = [
     {
         theme: ContainerWhite,
         border: BorderBlue,
-        title: 'Sorvete de Casquinha',
+        btn: ProductBtn,
+        btnArea: ProductBtnArea,
+        category: 'Sorvete de Casquinha',
         products: [{
+            id: 1,
             img: sorvete1,
             alt: 'Milk Shek',
             name: 'Name 01',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 2,
             img: sorvete2,
             alt: 'Morango com creme',
             name: 'Morango com creme',
             price: 'R$9.99'
         },
         {
+            id: 3,
             img: sorvete3,
             alt: 'Morango',
             name: 'Morango',
@@ -132,10 +265,18 @@ export const sections = [
         }
         ,
         {
+            id: 4,
             img: sorvete4,
             alt: 'Milk Shek',
             name: 'Name 04',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         }
         ],
         allProductsUrl: '/',
@@ -144,24 +285,46 @@ export const sections = [
     {
         theme: ContainerPink,
         border: BorderWhite,
-        title: 'Sorvete de Pote',
+        btn: ProductBtn2,
+        btnArea: ProductBtnArea,
+        category: 'Picolé',
         products: [{
+            id: 1,
             img: picole1,
             alt: 'Limão com hortelã',
             name: 'Limão com hortelã',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                
+
         },
         {
+            id: 2,
             img: picole2,
             alt: 'Chocolate com castanha',
             name: 'Chocolate com castanha',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+               
+
         },
         {
+            id: 3,
             img: picole3,
             alt: 'Melância',
             name: 'Melância',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+               
+
         }
         ],
         allProductsUrl: '/',
@@ -170,30 +333,68 @@ export const sections = [
     {
         theme: ContainerWhite,
         border: BorderPink,
-        title: 'Sundae',
+        btn: ProductBtn,
+        btnArea: ProductBtnArea,
+        category: 'Sundae',
         products: [{
+            id: 1,
+
             img: sundae1,
             alt: 'Brownie',
             name: 'Brownie',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 2,
+
             img: sundae2,
             alt: 'Milk Shek',
             name: 'Name 02',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 3,
+
             img: sundae3,
             alt: 'Cherry Bakewell',
             name: 'Cherry Bakewell',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         },
         {
+            id: 4,
+
             img: sundae4,
             alt: 'Raspberry',
             name: 'Raspberry',
-            price: 'R$19.99'
+            price: 'R$19.99',
+             ing1:'Maçã',
+                ing2:'Perâ',
+                ing3:'Uva',
+                ing4:'Leite',
+                ing5:'Cereja',
+                ing6:'Biscoito',
+
         }
         ],
         allProductsUrl: '/',
@@ -202,3 +403,5 @@ export const sections = [
 
 ];
 
+
+export default sections;

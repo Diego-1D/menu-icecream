5b4b139665221a73e7e68cfdb837ff7b7c4d9772
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-10-05 00:03:30 -0300

    Criação do botão voltar da página de Menu

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -5,17 +5,20 @@ import items from '../Home/content';
 
 import {
     MenuContainer,
+    BackButton,
     MenuTitle,
     H2,
     BorderPink,
+    BackButtonIcon,
 } from './styles';
+import { Link } from 'react-router-dom';
 
 const allCategories = ['Todos', ...new Set(items.map((item) => item.category))];
 
 const Menu = () => {
-    const [menuItems, setMenuItems] = useState (items);
+    const [menuItems, setMenuItems] = useState(items);
     const [categories, setCategories] = useState(allCategories)
-    
+
     const filterItems = (category) => {
         if (category === 'Todos') {
             setMenuItems(items);
@@ -24,17 +27,22 @@ const Menu = () => {
         const newItems = items.filter((items) => items.category === category);
         setMenuItems(newItems)
     }
-    
+
     return (
         <MenuContainer>
-            
             <BorderPink>
+                <Link to='/'>
+                    <BackButtonIcon/>
+                </Link>
+                <BackButton to='/'>
+                    Voltar para Home
+                </BackButton>
                 <MenuTitle>
-                <H2>Menu</H2>
+                    <H2>Menu</H2>
                 </MenuTitle>
-           </BorderPink>
-            <Categories categories={categories} filterItems={filterItems}/>
-            <SectionMenu items={menuItems}/>
+            </BorderPink>
+            <Categories categories={categories} filterItems={filterItems} />
+            <SectionMenu items={menuItems} />
         </MenuContainer>
     )
 

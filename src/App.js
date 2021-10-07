237dd772ce142814a6b306ca929929f58109e50c
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2021-10-06 21:03:17 -0300

    Alteração

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Menu from './pages/Menu';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <Switch>
         <Route exact path='/'>
